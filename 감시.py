d22eaeffec300e5fa17c1b02345b79a704ee0006
Kimjimin97 <sja9707@daum.net>
2022-06-15 23:56:07 +0900

    algorithm

--- 감시.py
+++ 감시.py
@@ -11,7 +11,58 @@ for i in range(N):
             cctv.append((i,j))
 
 
+dxy = [[0,1],[1,0],[0,-1],[-1,0]]
 
 move = dict()
-move[1] = [(0),(1),(2),(3)]
-move[2] = 
+move[1] = [[0],[1],[2],[3]]
+move[2] = [[0,2],[1,3]]
+move[3] = [[0,1],[1,2],[2,3],[0,3]]
+move[4] = [[0,1,2],[1,2,3],[2,3,0],[3,0,1]]
+move[5] = [[0,1,2,3]]
+
+print(cctv)
+print(move)
+
+print(move[1])
+print(move[2])
+
+for d in move[2]:
+    for dd in d:
+        print(dd)
+
+# res = float("Inf")
+
+# def dfs(L,x,y,n):
+#     global res
+#     if L == len(cctv):
+#         res = min(res, n)
+#         return
+    
+#     if n >= res:
+#         return
+    
+#     print(move[graph[x][y]])
+#     for dir in move[graph[x][y]]:
+#         visited = []
+#         mx, my = x, y
+#         for dd in dir:
+
+#             for d in range(len(dd)):
+#                 while True:
+#                     nx, ny = mx+dxy[dd[d]][0], my +dxy[dd[d]][0]
+#                     if  0 > nx or 0> ny or nx >= N or ny >=M:
+#                         break
+#                     if graph[nx][ny] != 0:
+#                         break
+
+#                     n += 1
+#                     graph[nx][ny] = "#"
+#                     visited.append([nx,ny])
+#         dfs(L+1,cctv[L+1][0], cctv[L+1][1],n)
+
+#         for a,b in visited:
+#             graph[a][b] = "0"
+
+
+# dfs(0,cctv[0][0],cctv[0][1],0)
+# print(res)
