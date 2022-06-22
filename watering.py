b727a9b2aa108232f45b4511e715969bf705dd80
Kimjimin97 <sja9707@daum.net>
2022-06-22 22:35:04 +0900

    수식 최대화 알고리즘

---
+++ watering.py
@@ -0,0 +1,55 @@
+import sys
+import heapq
+input = sys.stdin.readline
+N, C = map(int, input().split())
+
+
+nodes = []
+node = []
+for _ in range(N):
+    a,b = map(int, input().split())
+    node.append([a,b])
+
+
+
+for i in range(len(node)):
+    a,b = node[i][0], node[i][1]
+    for j in range(i+1,len(node)):
+        x,y = node[j][0], node[j][1]
+        dis = (a-x)**2 + (b-y)**2
+
+        if dis > C:
+            heapq.heappush(nodes,[dis, i,j])
+
+
+def find(kk):
+    if parent[kk] != kk:
+        parent[kk] = find(parent[kk])
+    return parent[kk]
+
+def union(aa,bb):
+    root1 = find(aa)
+    root2 = find(bb)
+    parent[root2] = root1
+
+
+
+parent = [i for i in range(N)]
+edge = 0
+
+cost = 0
+while nodes:
+    top = heapq.heappop(nodes)
+
+    c,aa,bb = top[0], top[1], top[2]
+    if find(aa) != find(bb):
+        union(aa,bb)
+        cost += c
+        edge+= 1
+        if edge == N-1:
+            break
+
+if cost == 0:
+    print(-1)
+else:
+    print(cost)
---
+++ 수식최대화.py
@@ -0,0 +1,61 @@
+from itertools import permutations
+from collections import deque
+import copy
+def solution(expression):
+    before = deque()
+    after = deque()
+    
+    items = list(permutations(["+","-","*"]))
+    
+    
+    int_str = ""
+    origin = deque()
+    for s in expression:
+        if s in "-+*":
+            origin.append(int(int_str))
+            origin.append(s)
+            int_str = ""
+        else:
+            int_str += s
+            
+    origin.append(int(int_str))
+
+
+    def calculate(left, right, oper):
+        print(left, right)
+        if oper == "+":
+            value = left + right
+            return value
+        elif oper =="-":
+            value = left - right
+            return value
+        elif oper =="*":
+            value = left * right
+            return value
+    answer = float("-Inf")
+    
+
+    
+    for it in items:
+        after = origin.copy()
+        for oper in it:
+            cnt = 0
+            before = after.copy()
+            after = deque()
+            max_len =len(before)
+            while cnt < max_len:
+                left = before.popleft()
+                cnt += 1
+                if left == oper:
+                    after.append(calculate(after.pop(), before.popleft(),left))
+                    cnt += 1
+                else:
+                    after.append(left)
+                    
+                    
+        
+        answer = max(answer, after[0])
+    return 
+
+
+solution("100-200*300-500+20")
